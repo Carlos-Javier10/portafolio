c2cc493a33dc97e1887b9c903b32438aa628fb28
Carlos-Javier10 <carlos.guagrilla@hotmail.com>
2025-03-09 23:18:24 -0500

    segunda

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/footerStyle.css'; // Importa el archivo CSS
+
 const Footer = () => {
-    return (
-      <footer className="bg-gray-900 text-white text-center p-4 mt-10">
-        <p>&copy; {new Date().getFullYear()} Mi Portafolio | Todos los derechos reservados</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className="bg-gray-900 text-white text-center p-4 mt-10">
+      <p>&copy; {new Date().getFullYear()} Mi Portafolio | Todos los derechos reservados</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import '../styles/headerStyle.css'; // Importa el archivo CSS
 
 const Header = () => {
   return (
